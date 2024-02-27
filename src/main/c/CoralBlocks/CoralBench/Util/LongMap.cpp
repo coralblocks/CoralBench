4b680f08bae34accc055d75a3e9880274087fb6e
Sergio Oliveira <sergio.oliveira@coralblocks.com>
2024-02-27 15:52:35 -0500

    Adding namespace

--- src/main/c/CoralBlocks/CoralBench/Util/LongMap.cpp
+++ src/main/c/CoralBlocks/CoralBench/Util/LongMap.cpp
@@ -10,289 +10,293 @@ using std::to_string;
 using std::round;
 using std::runtime_error;
 
-template <typename E>
-LongMap<E>::LongMap(int initialCapacity, float loadFactor) {
+namespace CoralBlocks::CoralBench::Util {
 
-    if (!MathUtils::isPowerOfTwo(initialCapacity)) {
-        throw invalid_argument("Size must be power of two: " + to_string(initialCapacity));
+    template <typename E>
+    LongMap<E>::LongMap(int initialCapacity, float loadFactor) {
+
+        if (!MathUtils::isPowerOfTwo(initialCapacity)) {
+            throw invalid_argument("Size must be power of two: " + to_string(initialCapacity));
+        }
+
+        data = new Entry*[initialCapacity]();
+        lengthMinusOne = initialCapacity - 1;
+        this->loadFactor = loadFactor;
+        threshold = static_cast<int>(round(initialCapacity * loadFactor));
+        reusableIter = new ReusableIterator(this);
     }
 
-    data = new Entry*[initialCapacity]();
-    lengthMinusOne = initialCapacity - 1;
-    this->loadFactor = loadFactor;
-    threshold = static_cast<int>(round(initialCapacity * loadFactor));
-    reusableIter = new ReusableIterator(this);
-}
+    template <typename E>
+    LongMap<E>::~LongMap() {
 
-template <typename E>
-LongMap<E>::~LongMap() {
+        clear();
 
-    clear();
+        // don't forget to release all entry objects from the pool...
+        Entry* entryToRelease = nullptr;
+        while((entryToRelease = poolHead) != nullptr) {
+            Entry* nextPtr = entryToRelease->next;
+            delete entryToRelease;
+            poolHead = nextPtr;
+        }
 
-    // don't forget to release all entry objects from the pool...
-    Entry* entryToRelease = nullptr;
-    while((entryToRelease = poolHead) != nullptr) {
-        Entry* nextPtr = entryToRelease->next;
-        delete entryToRelease;
-        poolHead = nextPtr;
+        delete[] data;
+        delete reusableIter;
     }
 
-    delete[] data;
-    delete reusableIter;
-}
+    template <typename E>
+    int LongMap<E>::size() const {
+        return count;
+    }
 
-template <typename E>
-int LongMap<E>::size() const {
-    return count;
-}
+    template <typename E>
+    bool LongMap<E>::isEmpty() const {
+        return size() == 0;
+    }
 
-template <typename E>
-bool LongMap<E>::isEmpty() const {
-    return size() == 0;
-}
+    template <typename E>
+    bool LongMap<E>::contains(const E& value) const {
+        for (int i = lengthMinusOne; i >= 0; i--) {
+            Entry* e = data[i];
+            while (e != nullptr) {
+                if (e->value == value) {
+                    return true;
+                }
+                e = e->next;
+            }
+        }
+        return false;
+    }
 
-template <typename E>
-bool LongMap<E>::contains(const E& value) const {
-    for (int i = lengthMinusOne; i >= 0; i--) {
-        Entry* e = data[i];
+    template <typename E>
+    bool LongMap<E>::containsKey(long key) const {
+        Entry* e = data[toArrayIndex(key)];
         while (e != nullptr) {
-            if (e->value == value) {
+            if (e->key == key) {
                 return true;
             }
             e = e->next;
         }
+        return false;
     }
-    return false;
-}
 
-template <typename E>
-bool LongMap<E>::containsKey(long key) const {
-    Entry* e = data[toArrayIndex(key)];
-    while (e != nullptr) {
-        if (e->key == key) {
-            return true;
+    template <typename E>
+    E LongMap<E>::get(long key) const {
+        Entry* e = data[toArrayIndex(key)];
+        while (e != nullptr) {
+            if (e->key == key) {
+                return e->value;
+            }
+            e = e->next;
         }
-        e = e->next;
+        return nullptr;
     }
-    return false;
-}
 
-template <typename E>
-E LongMap<E>::get(long key) const {
-    Entry* e = data[toArrayIndex(key)];
-    while (e != nullptr) {
-        if (e->key == key) {
-            return e->value;
+    template <typename E>
+    E LongMap<E>::put(long key, const E& value) {
+
+        if (value == nullptr) {
+            throw invalid_argument("Cannot put null value!");
         }
-        e = e->next;
-    }
-    return nullptr;
-}
 
-template <typename E>
-E LongMap<E>::put(long key, const E& value) {
+        int index = toArrayIndex(key);
 
-    if (value == nullptr) {
-        throw invalid_argument("Cannot put null value!");
-    }
+        Entry* e = data[index];
 
-    int index = toArrayIndex(key);
-
-    Entry* e = data[index];
-
-    while (e != nullptr) {
-        if (e->key == key) {
-            E old = e->value;
-            e->value = value;
-            return old;
+        while (e != nullptr) {
+            if (e->key == key) {
+                E old = e->value;
+                e->value = value;
+                return old;
+            }
+            e = e->next;
         }
-        e = e->next;
+
+        if (count >= threshold) {
+            rehash();
+            index = toArrayIndex(key); // lengthMinusOne has changed!
+        }
+        
+        data[index] = getEntryFromPool(key, value, data[index]);
+
+        count++;
+
+        return nullptr;
     }
 
-    if (count >= threshold) {
-        rehash();
-        index = toArrayIndex(key); // lengthMinusOne has changed!
-    }
-    
-    data[index] = getEntryFromPool(key, value, data[index]);
+    template <typename E>
+    E LongMap<E>::remove(long key) {
 
-    count++;
+        int index = toArrayIndex(key);
 
-    return nullptr;
-}
+        Entry* e = data[index];
+        Entry* prev = nullptr;
 
-template <typename E>
-E LongMap<E>::remove(long key) {
+        while (e != nullptr) {
+            if (e->key == key) {
+                if (prev != nullptr) {
+                    prev->next = e->next;
+                } else {
+                    data[index] = e->next;
+                }
 
-    int index = toArrayIndex(key);
+                E oldValue = e->value;
+                releaseEntryBackToPool(e);
+                count--;
 
-    Entry* e = data[index];
-    Entry* prev = nullptr;
-
-    while (e != nullptr) {
-        if (e->key == key) {
-            if (prev != nullptr) {
-                prev->next = e->next;
-            } else {
-                data[index] = e->next;
+                return oldValue;
             }
 
-            E oldValue = e->value;
-            releaseEntryBackToPool(e);
-            count--;
-
-            return oldValue;
+            prev = e;
+            e = e->next;
         }
 
-        prev = e;
-        e = e->next;
+        return nullptr;
     }
 
-    return nullptr;
-}
-
-template <typename E>
-void LongMap<E>::clear() {
-    for (int index = lengthMinusOne; index >= 0; index--) {
-        while (data[index] != nullptr) {
-            Entry* next = data[index]->next;
-            releaseEntryBackToPool(data[index]);
-            data[index] = next;
+    template <typename E>
+    void LongMap<E>::clear() {
+        for (int index = lengthMinusOne; index >= 0; index--) {
+            while (data[index] != nullptr) {
+                Entry* next = data[index]->next;
+                releaseEntryBackToPool(data[index]);
+                data[index] = next;
+            }
         }
+
+        count = 0;
     }
 
-    count = 0;
-}
+    template <typename E>
+    void LongMap<E>::rehash() {
 
-template <typename E>
-void LongMap<E>::rehash() {
+        int oldCapacity = lengthMinusOne + 1;
 
-    int oldCapacity = lengthMinusOne + 1;
+        Entry** oldData = data;
 
-    Entry** oldData = data;
+        int newCapacity = oldCapacity * 2; // power of two, always!
 
-    int newCapacity = oldCapacity * 2; // power of two, always!
+        data = new Entry*[newCapacity]();
+        lengthMinusOne = newCapacity - 1;
 
-    data = new Entry*[newCapacity]();
-    lengthMinusOne = newCapacity - 1;
+        threshold = static_cast<int>(round(newCapacity * loadFactor));
 
-    threshold = static_cast<int>(round(newCapacity * loadFactor));
+        for (int i = oldCapacity - 1; i >= 0; i--) {
 
-    for (int i = oldCapacity - 1; i >= 0; i--) {
+            Entry* old = oldData[i];
 
-        Entry* old = oldData[i];
+            while (old != nullptr) {
+                Entry* e = old;
+                old = old->next;
 
-        while (old != nullptr) {
-            Entry* e = old;
-            old = old->next;
-
-            int index = toArrayIndex(e->key);
-            e->next = data[index];
-            data[index] = e;
+                int index = toArrayIndex(e->key);
+                e->next = data[index];
+                data[index] = e;
+            }
         }
+
+        delete[] oldData; // don't forget
     }
 
-    delete[] oldData; // don't forget
-}
+    template <typename E>
+    typename LongMap<E>::Entry* LongMap<E>::getEntryFromPool(long key, E value, Entry* next) {
 
-template <typename E>
-typename LongMap<E>::Entry* LongMap<E>::getEntryFromPool(long key, E value, Entry* next) {
+        Entry* newEntry = poolHead;
 
-    Entry* newEntry = poolHead;
-
-    if (newEntry != nullptr) {
-        poolHead = newEntry->next;
-    } else {
-        newEntry = new Entry();
-    }
-
-    newEntry->key = key;
-    newEntry->value = value;
-    newEntry->next = next;
-
-    return newEntry;
-}
-
-template <typename E>
-void LongMap<E>::releaseEntryBackToPool(Entry* e) {
-    e->value = nullptr;
-    e->next = poolHead;
-    poolHead = e;
-}
-
-template <typename E>
-int LongMap<E>::toArrayIndex(long key) const {
-    return (static_cast<unsigned int>(key) & 0x7FFFFFFF) & lengthMinusOne;
-}
-
-template <typename E>
-typename LongMap<E>::ReusableIterator LongMap<E>::iterator() {
-    reusableIter->reset();
-    return reusableIter;
-}
-
-template <typename E>
-void LongMap<E>::ReusableIterator::reset() {
-    size = outer->count;
-    index = 0;
-    dataIndex = 0;
-    prev = nullptr;
-    next = outer->data[0];
-    entry = nullptr;
-    wasRemoved = false;
-}
-
-template <typename E>
-bool LongMap<E>::ReusableIterator::hasNext() const {
-    return index < size;
-}
-
-template <typename E>
-E LongMap<E>::ReusableIterator::nextValue() {
-
-    if (index >= size) throw runtime_error("nothing to return");
-
-    if (!wasRemoved) prev = entry;
-    
-    wasRemoved = false;
-
-    entry = next;
-
-    if (entry == nullptr) {
-        while(entry == nullptr) {
-            dataIndex++;
-            entry = outer->data[dataIndex];
+        if (newEntry != nullptr) {
+            poolHead = newEntry->next;
+        } else {
+            newEntry = new Entry();
         }
+
+        newEntry->key = key;
+        newEntry->value = value;
+        newEntry->next = next;
+
+        return newEntry;
+    }
+
+    template <typename E>
+    void LongMap<E>::releaseEntryBackToPool(Entry* e) {
+        e->value = nullptr;
+        e->next = poolHead;
+        poolHead = e;
+    }
+
+    template <typename E>
+    int LongMap<E>::toArrayIndex(long key) const {
+        return (static_cast<unsigned int>(key) & 0x7FFFFFFF) & lengthMinusOne;
+    }
+
+    template <typename E>
+    typename LongMap<E>::ReusableIterator LongMap<E>::iterator() {
+        reusableIter->reset();
+        return reusableIter;
+    }
+
+    template <typename E>
+    void LongMap<E>::ReusableIterator::reset() {
+        size = outer->count;
+        index = 0;
+        dataIndex = 0;
         prev = nullptr;
+        next = outer->data[0];
+        entry = nullptr;
+        wasRemoved = false;
     }
 
-    index++;
-    
-    E o = entry->value;
-
-    outer->currIteratorKey = entry->key;
-
-    next = entry->next;
-
-    return o;
-}
-
-template <typename E>
-void LongMap<E>::ReusableIterator::remove() {
-
-    if (wasRemoved || entry == nullptr) throw runtime_error("nothing to remove");
-    
-    wasRemoved = true;
-
-    if (prev == nullptr) {
-        outer->data[dataIndex] = next;
-    } else {
-        prev->next = next;
+    template <typename E>
+    bool LongMap<E>::ReusableIterator::hasNext() const {
+        return index < size;
     }
 
-    outer->releaseEntryBackToPool(entry);
+    template <typename E>
+    E LongMap<E>::ReusableIterator::nextValue() {
 
-    entry = nullptr;
+        if (index >= size) throw runtime_error("nothing to return");
 
-    outer->count--;
+        if (!wasRemoved) prev = entry;
+        
+        wasRemoved = false;
+
+        entry = next;
+
+        if (entry == nullptr) {
+            while(entry == nullptr) {
+                dataIndex++;
+                entry = outer->data[dataIndex];
+            }
+            prev = nullptr;
+        }
+
+        index++;
+        
+        E o = entry->value;
+
+        outer->currIteratorKey = entry->key;
+
+        next = entry->next;
+
+        return o;
+    }
+
+    template <typename E>
+    void LongMap<E>::ReusableIterator::remove() {
+
+        if (wasRemoved || entry == nullptr) throw runtime_error("nothing to remove");
+        
+        wasRemoved = true;
+
+        if (prev == nullptr) {
+            outer->data[dataIndex] = next;
+        } else {
+            prev->next = next;
+        }
+
+        outer->releaseEntryBackToPool(entry);
+
+        entry = nullptr;
+
+        outer->count--;
+    }
+
 }
--- src/main/c/CoralBlocks/CoralBench/Util/LongMap.h
+++ src/main/c/CoralBlocks/CoralBench/Util/LongMap.h
@@ -3,86 +3,89 @@
 #include <iterator>
 #include <stdexcept>
 
-template <typename E>
-class LongMap {
+namespace CoralBlocks::CoralBench::Util {
 
-    private:
-        static const int DEFAULT_INITIAL_CAPACITY = 128;
-        static constexpr float DEFAULT_LOAD_FACTOR = 0.80f;
-
-        struct Entry {
-            long key;
-            E value;
-            Entry* next;
-        };
-
-        Entry** data;
-        int lengthMinusOne;
-        int count;
-        int threshold;
-        float loadFactor;
-        Entry* poolHead;
-        long currIteratorKey;
-
-        void rehash();
-
-        Entry* getEntryFromPool(long key, E value, Entry* next);
-
-        void releaseEntryBackToPool(Entry* e);
-
-        int toArrayIndex(long key) const;
-
-    public:
-        LongMap() : LongMap(DEFAULT_INITIAL_CAPACITY, DEFAULT_LOAD_FACTOR) {}
-        LongMap(int initialCapacity) : LongMap(initialCapacity, DEFAULT_LOAD_FACTOR) {}
-        LongMap(int initialCapacity, float loadFactor);
-        ~LongMap();
-
-        LongMap(const LongMap&) = delete;
-        LongMap& operator=(const LongMap&) = delete;
-        LongMap(LongMap&&) = delete;
-        LongMap& operator=(LongMap&&) = delete;
-
-        int size() const;
-        bool isEmpty() const;
-        bool contains(const E& value) const;
-        bool containsKey(long key) const;
-        E get(long key) const;
-        E put(long key, const E& value);
-        E remove(long key);
-        void clear();
-
-        class ReusableIterator {
-
-            private:
-                LongMap<E>* outer;
-                int size;
-                int index;
-                int dataIndex;
-                Entry* prev;
-                Entry* next;
-                Entry* entry;
-                bool wasRemoved;
-
-            public:
-                ReusableIterator(LongMap<E>& outerPtr) : outer(outerPtr) { }
-                ~ReusableIterator() = default;
-
-                ReusableIterator(const ReusableIterator&) = delete;
-                ReusableIterator& operator=(const ReusableIterator&) = delete;
-                ReusableIterator(ReusableIterator&&) = delete;
-                ReusableIterator& operator=(ReusableIterator&&) = delete;
-
-                void reset();
-                bool hasNext() const;
-                E nextValue();
-                void remove();
-        };
-
-        ReusableIterator iterator();
+    template <typename E>
+    class LongMap {
 
         private:
+            static const int DEFAULT_INITIAL_CAPACITY = 128;
+            static constexpr float DEFAULT_LOAD_FACTOR = 0.80f;
 
-            ReusableIterator* reusableIter; 
+            struct Entry {
+                long key;
+                E value;
+                Entry* next;
+            };
 
-};
+            Entry** data;
+            int lengthMinusOne;
+            int count;
+            int threshold;
+            float loadFactor;
+            Entry* poolHead;
+            long currIteratorKey;
+
+            void rehash();
+
+            Entry* getEntryFromPool(long key, E value, Entry* next);
+
+            void releaseEntryBackToPool(Entry* e);
+
+            int toArrayIndex(long key) const;
+
+        public:
+            LongMap() : LongMap(DEFAULT_INITIAL_CAPACITY, DEFAULT_LOAD_FACTOR) {}
+            LongMap(int initialCapacity) : LongMap(initialCapacity, DEFAULT_LOAD_FACTOR) {}
+            LongMap(int initialCapacity, float loadFactor);
+            ~LongMap();
+
+            LongMap(const LongMap&) = delete;
+            LongMap& operator=(const LongMap&) = delete;
+            LongMap(LongMap&&) = delete;
+            LongMap& operator=(LongMap&&) = delete;
+
+            int size() const;
+            bool isEmpty() const;
+            bool contains(const E& value) const;
+            bool containsKey(long key) const;
+            E get(long key) const;
+            E put(long key, const E& value);
+            E remove(long key);
+            void clear();
+
+            class ReusableIterator {
+
+                private:
+                    LongMap<E>* outer;
+                    int size;
+                    int index;
+                    int dataIndex;
+                    Entry* prev;
+                    Entry* next;
+                    Entry* entry;
+                    bool wasRemoved;
+
+                public:
+                    ReusableIterator(LongMap<E>& outerPtr) : outer(outerPtr) { }
+                    ~ReusableIterator() = default;
+
+                    ReusableIterator(const ReusableIterator&) = delete;
+                    ReusableIterator& operator=(const ReusableIterator&) = delete;
+                    ReusableIterator(ReusableIterator&&) = delete;
+                    ReusableIterator& operator=(ReusableIterator&&) = delete;
+
+                    void reset();
+                    bool hasNext() const;
+                    E nextValue();
+                    void remove();
+            };
+
+            ReusableIterator iterator();
+
+            private:
+
+                ReusableIterator* reusableIter; 
+
+    };
+}
