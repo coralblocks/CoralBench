257f3e25bc594cbe42c47031db0c4ec3846eddeb
Sergio Oliveira <sergio.oliveira@coralblocks.com>
2024-02-28 18:30:32 -0500

    Formatting

--- src/main/java/com/coralblocks/coralbench/util/FastObjectList.java
+++ src/main/java/com/coralblocks/coralbench/util/FastObjectList.java
@@ -19,56 +19,56 @@ import java.util.Iterator;
 import java.util.NoSuchElementException;
 
 public class FastObjectList<E> implements Iterable<E> {
-	
+
 	private E[] values;
 	private int pointer = -1;
 	private int removed = 0;
 	private final ReusableIterator iter = new ReusableIterator();
-	
+
 	@SuppressWarnings("unchecked")
-    	public FastObjectList(int initialCapacity) {
+	public FastObjectList(int initialCapacity) {
 		this.values = (E[]) new Object[initialCapacity];
 	}
-	
+
 	public final int size() {
 		return pointer + 1 - removed;
 	}
-	
+
 	public final int getCapacity() {
 		return values.length;
 	}
-	
+
 	public final boolean isEmpty() {
 		return size() == 0;
 	}
-	
+
 	public final boolean isAtCapacity() {
 		return pointer + 1 == values.length;
 	}
-	
+
 	public final boolean add(E value) {
 		if (pointer + 1 == values.length) return false;
 		this.values[++pointer] = value;
 		return true;
 	}
-	
+
 	@SuppressWarnings("unchecked")
 	public int grow() {
-		
+
 		int newCapacity = values.length + values.length / 2;
-		
-        E[] newValues = (E[]) new Object[newCapacity];
-		
+
+		E[] newValues = (E[]) new Object[newCapacity];
+
 		System.arraycopy(values, 0, newValues, 0, values.length);
-		
+
 		this.values = newValues;
-		
+
 		return newCapacity;
 	}
-	
+
 	public final E removeLast() {
 		if (isEmpty()) throw new NoSuchElementException();
-		for(int i = pointer; i >= 0; i--) {
+		for (int i = pointer; i >= 0; i--) {
 			if (values[i] != null) {
 				E old = values[i];
 				values[i] = null;
@@ -78,10 +78,10 @@ public class FastObjectList<E> implements Iterable<E> {
 		}
 		throw new IllegalStateException("Should never happen!");
 	}
-	
+
 	public final E removeFirst() {
 		if (isEmpty()) throw new NoSuchElementException();
-		for(int i = 0; i <= pointer; i++) {
+		for (int i = 0; i <= pointer; i++) {
 			if (values[i] != null) {
 				E save = values[i];
 				values[i] = null;
@@ -91,41 +91,41 @@ public class FastObjectList<E> implements Iterable<E> {
 		}
 		throw new IllegalStateException("Should never happen!");
 	}
-	
+
 	public final boolean remove(E value) {
 		return remove(value, false);
 	}
-	
+
 	public final boolean remove(E value, boolean fromTail) {
 		if (!fromTail) {
-    		for(int i = 0; i <= pointer; i++) {
-    			if (values[i] == value) {
-    				values[i] = null;
-    				removed++;
-    				return true;
-    			}
-    		}
+			for (int i = 0; i <= pointer; i++) {
+				if (values[i] == value) {
+					values[i] = null;
+					removed++;
+					return true;
+				}
+			}
 		} else {
-			for(int i = pointer; i >= 0; i--) {
-    			if (values[i] == value) {
-    				values[i] = null;
-    				removed++;
-    				return true;
-    			}
-    		}
+			for (int i = pointer; i >= 0; i--) {
+				if (values[i] == value) {
+					values[i] = null;
+					removed++;
+					return true;
+				}
+			}
 		}
 		return false;
 	}
-	
+
 	public final E remove(int index) {
 		return remove(index, false);
 	}
-	
+
 	public final E remove(int index, boolean fromTail) {
 		if (index >= size()) throw new NoSuchElementException("Cannot remove index " + index + " from a list of size " + size() + " (fromTail=" + fromTail + ")");
 		if (!fromTail) {
 			int count = -1;
-			for(int i = 0; i <= pointer; i++) {
+			for (int i = 0; i <= pointer; i++) {
 				if (values[i] != null && ++count == index) {
 					E save = values[i];
 					values[i] = null;
@@ -135,55 +135,55 @@ public class FastObjectList<E> implements Iterable<E> {
 			}
 		} else {
 			int count = -1;
-			for(int i = pointer; i >= 0; i--) {
-    			if (values[i] != null && ++count == index) {
-    				E save = values[i];
-    				values[i] = null;
-    				removed++;
-    				return save;
-    			}
-    		}
+			for (int i = pointer; i >= 0; i--) {
+				if (values[i] != null && ++count == index) {
+					E save = values[i];
+					values[i] = null;
+					removed++;
+					return save;
+				}
+			}
 		}
 		return null;
 	}
-	
+
 	public final E first() {
 		if (isEmpty()) throw new NoSuchElementException();
-		for(int i = 0; i <= pointer; i++) {
+		for (int i = 0; i <= pointer; i++) {
 			if (values[i] != null) {
 				return values[i];
 			}
 		}
 		throw new IllegalStateException("Should never happen!");
 	}
-	
+
 	public final E last() {
 		if (isEmpty()) throw new NoSuchElementException();
-		for(int i = pointer; i >= 0; i--) {
+		for (int i = pointer; i >= 0; i--) {
 			if (values[i] != null) {
 				return values[i];
 			}
 		}
 		throw new IllegalStateException("Should never happen!");
 	}
-	
+
 	public final void clear() {
 		pointer = -1;
 		removed = 0;
 	}
-	
+
 	@Override
-    public Iterator<E> iterator() {
-	    iter.reset();
-	    return iter;
-    }
-	
+	public Iterator<E> iterator() {
+		iter.reset();
+		return iter;
+	}
+
 	private class ReusableIterator implements Iterator<E> {
-		
+
 		private int pointer;
 		private int size;
 		private int returned;
-		
+
 		public void reset() {
 			this.size = FastObjectList.this.size();
 			this.pointer = 0;
@@ -191,21 +191,21 @@ public class FastObjectList<E> implements Iterable<E> {
 		}
 
 		@Override
-        public boolean hasNext() {
-	        return this.returned < size;
-        }
+		public boolean hasNext() {
+			return this.returned < size;
+		}
 
 		@Override
-        public E next() {
+		public E next() {
 			E value;
-			while((value = FastObjectList.this.values[this.pointer++]) == null);
+			while ((value = FastObjectList.this.values[this.pointer++]) == null);
 			this.returned++;
 			return value;
-        }
+		}
 
 		@Override
-        public void remove() {
+		public void remove() {
 			throw new UnsupportedOperationException();
-        }
+		}
 	}
 }
