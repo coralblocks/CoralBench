6d1b670f6574327f67ccc7b5c3a56d719b7b473b
Sergio Oliveira <sergio.oliveira@coralblocks.com>
2024-03-01 12:24:40 -0500

    Fix bug

--- bin/runJavaLongMapBench.sh
+++ bin/runJavaLongMapBench.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
 ITERATIONS=${1:-100000}
-PASSES=${1:-6}
-CORE_ID=${1:--1}
+PASSES=${2:-6}
+CORE_ID=${3:--1}
 
 CMD="java -verbose:gc -Xms2g -Xmx4g -XX:NewSize=512m -XX:MaxNewSize=1024m -cp target/coralbench-all.jar com.coralblocks.coralbench.bench.LongMapBench $ITERATIONS $PASSES $CORE_ID"
 
