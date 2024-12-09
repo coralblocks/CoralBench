6ae5d6c1741c84ea3cfe6aa1b7a92c230ceb47fd
Sergio Oliveira <sergio.oliveira@coralblocks.com>
2024-12-09 11:44:23 -0300

    Remove -verbose:gc

--- bin/runIntMapBenchmarkJava.sh
+++ bin/runIntMapBenchmarkJava.sh
@@ -4,4 +4,4 @@ WARMUP=${1:-1000000}
 MEASUREMENTS=${2:-1000000}
 CAPACITY=${3:-100000}
 
-java -verbose:gc -XX:+AlwaysPreTouch -Xms4g -Xmx4g -XX:NewSize=512m -XX:MaxNewSize=1024m -cp target/classes:target/coralbench-all.jar com.coralblocks.coralbench.example.IntMapBenchmark $WARMUP $MEASUREMENTS $CAPACITY
+java -XX:+AlwaysPreTouch -Xms4g -Xmx4g -XX:NewSize=512m -XX:MaxNewSize=1024m -cp target/classes:target/coralbench-all.jar com.coralblocks.coralbench.example.IntMapBenchmark $WARMUP $MEASUREMENTS $CAPACITY
--- bin/runIntMapBenchmarkJavaXCOMP.sh
+++ bin/runIntMapBenchmarkJavaXCOMP.sh
@@ -4,4 +4,4 @@ WARMUP=${1:-1000000}
 MEASUREMENTS=${2:-1000000}
 CAPACITY=${3:-100000}
 
-java -verbose:gc -Xcomp -XX:-TieredCompilation -XX:+AlwaysPreTouch -Xms4g -Xmx4g -XX:NewSize=512m -XX:MaxNewSize=1024m -cp target/classes:target/coralbench-all.jar com.coralblocks.coralbench.example.IntMapBenchmark $WARMUP $MEASUREMENTS $CAPACITY
+java -Xcomp -XX:-TieredCompilation -XX:+AlwaysPreTouch -Xms4g -Xmx4g -XX:NewSize=512m -XX:MaxNewSize=1024m -cp target/classes:target/coralbench-all.jar com.coralblocks.coralbench.example.IntMapBenchmark $WARMUP $MEASUREMENTS $CAPACITY
