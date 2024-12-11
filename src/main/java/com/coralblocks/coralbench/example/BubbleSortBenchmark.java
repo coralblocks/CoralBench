f083c70b06bdee821a4e6ba6cf15ada1cf35fc55
Sergio Oliveira <sergio.oliveira@coralblocks.com>
2024-12-11 19:34:11 -0300

    boolean swap

--- src/main/java/com/coralblocks/coralbench/example/BubbleSortBenchmark.java
+++ src/main/java/com/coralblocks/coralbench/example/BubbleSortBenchmark.java
@@ -62,14 +62,14 @@ public class BubbleSortBenchmark {
      
     private static final void bubbleSort(int[] array, int size) {
         for(int i = 0; i < size; i++) {
-            int swaps = 0; // flag to detect any swap is there or not
+            boolean swaps = false; // flag to detect any swap is there or not
             for(int j = 0; j < size - i - 1; j++) {
                 if (array[j] > array[j + 1]) { // when the current item is bigger than next
                     swapping(array, j, j + 1);
-                    swaps = 1;
+                    swaps = true;
                 }
             }
-            if (swaps == 0) break; // No swap in this pass, so array is sorted
+            if (!swaps) break; // No swap in this pass, so array is sorted
         }
     }
      
