b55ae334ed84ccde5011af23f78e7e7bb476def8
Sergio Oliveira <sergio.oliveira@coralblocks.com>
2024-12-11 22:14:25 -0300

    Adding optimization options to the JVM

--- bin/runMathBenchmarkJava.sh
+++ bin/runMathBenchmarkJava.sh
@@ -3,5 +3,5 @@
 WARMUP=${1:-0}
 MEASUREMENTS=${2:-10000000}
 
-java -XX:+AlwaysPreTouch -Xms4g -Xmx4g -XX:NewSize=512m -XX:MaxNewSize=1024m -cp target/classes:target/coralbench-all.jar com.coralblocks.coralbench.example.MathBenchmark $WARMUP $MEASUREMENTS
+java -XX:MaxInlineSize=270 -XX:InlineSmallCode=2000 -XX:+AlwaysPreTouch -Xms4g -Xmx4g -XX:NewSize=512m -XX:MaxNewSize=1024m -cp target/classes:target/coralbench-all.jar com.coralblocks.coralbench.example.MathBenchmark $WARMUP $MEASUREMENTS
 
