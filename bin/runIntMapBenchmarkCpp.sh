2b9e093ee183f8ceaffc108a982154705b85d370
Sergio Oliveira <sergio.oliveira@coralblocks.com>
2024-12-12 21:03:54 -0500

    Adjustments for IntMap bench scripts

--- bin/runIntMapBenchmarkCpp.sh
+++ bin/runIntMapBenchmarkCpp.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
 WARMUP=${1:-0}
-MEASUREMENTS=${2:-10000000}
-CAPACITY=${3:-5000000}
+MEASUREMENTS=${2:-2000000}
+CAPACITY=${3:-20000}
 
 ./target/cpp/int_map_benchmark $WARMUP $MEASUREMENTS $CAPACITY
 
---
+++ bin/runIntMapBenchmarkJava-Xcomp-C2.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+WARMUP=${1:-0}
+MEASUREMENTS=${2:-2000000}
+CAPACITY=${3:-20000}
+
+java -XX:-UseJVMCICompiler -Xcomp -XX:-TieredCompilation -XX:+AlwaysPreTouch -Xms4g -Xmx4g -XX:NewSize=512m -XX:MaxNewSize=1024m -cp target/classes:target/coralbench-all.jar com.coralblocks.coralbench.example.IntMapBenchmark $WARMUP $MEASUREMENTS $CAPACITY
