40248e054d29abecdf16dbd705892d547770ff07
Sergio Oliveira <sergio.oliveira@coralblocks.com>
2024-12-14 06:59:45 -0300

    Better scripts to swtich between C2 and JVMCI

---
+++ bin/runMathBenchmarkJavaJVMCI.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+WARMUP=${1:-0}
+MEASUREMENTS=${2:-10000000}
+
+java -XX:+UnlockExperimentalVMOptions -XX:+UseJVMCICompiler -XX:+AlwaysPreTouch -Xms4g -Xmx4g -XX:NewSize=512m -XX:MaxNewSize=1024m -cp target/classes:target/coralbench-all.jar com.coralblocks.coralbench.example.MathBenchmark $WARMUP $MEASUREMENTS
+
