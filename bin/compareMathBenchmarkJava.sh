58888dd2c7b147fbbeaad8dec9479315ea31a2bc
Sergio Oliveira <sergio.oliveira@coralblocks.com>
2026-08-28 15:53:49 -0300

    Raise Java benchmark warm-up defaults

--- bin/compareMathBenchmarkJava.sh
+++ bin/compareMathBenchmarkJava.sh
@@ -5,11 +5,5 @@ PASSES=${1:-1000}
 echo "Regular JIT with warm-up"
 java -cp target/classes:target/coralbench-all.jar com.coralblocks.coralbench.example.MathBenchmark 1000000 $PASSES
 
-echo "Regular JIT without warm-up"
-java -cp target/classes:target/coralbench-all.jar com.coralblocks.coralbench.example.MathBenchmark 5 $PASSES
-
 echo "-Xcomp -XX:-TieredCompilation with warm-up"
 java -Xcomp -XX:-TieredCompilation -cp target/classes:target/coralbench-all.jar com.coralblocks.coralbench.example.MathBenchmark 1000000 $PASSES
-
-echo "-Xcomp -XX:-TieredCompilation without warm-up"
-java -Xcomp -XX:-TieredCompilation -cp target/classes:target/coralbench-all.jar com.coralblocks.coralbench.example.MathBenchmark 5 $PASSES
--- bin/runMathBenchmarkJavaC2.sh
+++ bin/runMathBenchmarkJavaC2.sh
@@ -1,7 +1,6 @@
 #!/bin/bash
 
-WARMUP=${1:-0}
+WARMUP=${1:-1000000}
 MEASUREMENTS=${2:-10000000}
 
 java -XX:+UnlockExperimentalVMOptions -XX:-UseJVMCICompiler -XX:+AlwaysPreTouch -Xms4g -Xmx4g -XX:NewSize=512m -XX:MaxNewSize=1024m -cp target/classes:target/coralbench-all.jar com.coralblocks.coralbench.example.MathBenchmark $WARMUP $MEASUREMENTS
-
