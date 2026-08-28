a2e2a461e4307c838a030cef44c97a8bab17180e
Sergio Oliveira <sergio.oliveira@coralblocks.com>
2026-08-28 12:36:18 -0300

    Fix Bubble Sort C++ object cleanup

--- bin/compileBubbleSortBenchmarkCpp.sh
+++ bin/compileBubbleSortBenchmarkCpp.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-rm -f target/cpp/bubble_sort_benchmark target/cpp/bench.o target/cpp/math_benchmark.o
+rm -f target/cpp/bubble_sort_benchmark target/cpp/bench.o target/cpp/bubble_sort_benchmark.o
 
 mkdir -p target/cpp
 
@@ -8,7 +8,6 @@ clang++ -Ofast -march=native -flto -std=c++17 -I./src/main/c -c ./src/main/c/ben
 clang++ -Ofast -march=native -flto -std=c++17 -I./src/main/c -c ./src/main/c/bubble_sort_benchmark.cpp -o ./target/cpp/bubble_sort_benchmark.o
 
 clang++ -Ofast -march=native -flto -std=c++17 -o ./target/cpp/bubble_sort_benchmark ./target/cpp/bench.o ./target/cpp/bubble_sort_benchmark.o
-
 
 
 
