28be12b15ec4297bc54fc6ed7a2af929947af711
Sergio Oliveira <sergio.oliveira@coralblocks.com>
2026-08-30 13:58:20 -0300

    Use native clang compiler

--- bin/compileBubbleSortBenchmarkGraal.sh
+++ bin/compileBubbleSortBenchmarkGraal.sh
@@ -4,4 +4,4 @@ rm -f target/graal/BubbleSortMapBenchmark
 
 mkdir -p target/graal
 
-native-image --gc=G1 -march=native -cp target/coralbench-all.jar com.coralblocks.coralbench.example.BubbleSortBenchmark -o target/graal/BubbleSortBenchmark --no-fallback -O3 --initialize-at-build-time
+native-image --native-compiler-path=/usr/bin/clang --gc=G1 -march=native -cp target/coralbench-all.jar com.coralblocks.coralbench.example.BubbleSortBenchmark -o target/graal/BubbleSortBenchmark --no-fallback -O3 --initialize-at-build-time
