0fd2a963ee243a4db12648bf8cb77894cc03958d
Sergio Oliveira <sergio.oliveira@coralblocks.com>
2026-08-30 19:42:20 -0300

    Remove deprecated --no-fallback

--- bin/compileIntMapBenchmarkGraal.sh
+++ bin/compileIntMapBenchmarkGraal.sh
@@ -4,5 +4,5 @@ rm -f target/graal/IntMapBenchmark
 
 mkdir -p target/graal
 
-native-image --gc=G1 -R:+AlwaysPreTouch -R:InitialHeapSize=4g -R:MaxHeapSize=4g -march=native -cp target/coralbench-all.jar com.coralblocks.coralbench.example.IntMapBenchmark -o target/graal/IntMapBenchmark --no-fallback -O3 --initialize-at-build-time
+native-image --gc=G1 -R:+AlwaysPreTouch -R:InitialHeapSize=4g -R:MaxHeapSize=4g -march=native -cp target/coralbench-all.jar com.coralblocks.coralbench.example.IntMapBenchmark -o target/graal/IntMapBenchmark -O3 --initialize-at-build-time
 
